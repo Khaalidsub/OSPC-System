6438015489021158c3927e346ac929c3e5e59037
khaalid subaan <khaalidsubaan@gmail.com>
2021-01-14 12:07:28 +0800

    refactor graphql type department

--- ospc-api/src/departments/entities/department.entity.ts
+++ ospc-api/src/departments/entities/department.entity.ts
@@ -2,17 +2,11 @@ import { ObjectType, Field } from '@nestjs/graphql';
 import { User } from '../../users/entities/user.entity';
 import { IDepartment } from '../types';
 
-@ObjectType()
+@ObjectType({ implements: IDepartment })
 export class Department implements IDepartment {
   @Field(() => String, { description: 'Example field (placeholder)' })
   id: string;
-  @Field(() => String, { description: 'Example field (placeholder)' })
   departmentName: string;
-  @Field(() => String, { description: 'Example field (placeholder)' })
   departmentDescription: string;
-  @Field(() => User, {
-    description: 'Example field (placeholder)',
-    nullable: true,
-  })
   departmentModerator?: User;
 }
--- ospc-api/src/departments/types/index.ts
+++ ospc-api/src/departments/types/index.ts
@@ -1,7 +1,16 @@
-import { IUser } from 'src/users/types';
+import { Field, InterfaceType } from '@nestjs/graphql';
+import { User } from '../../users/entities/user.entity';
+import { IUser } from '../../users/types';
 
-export interface IDepartment {
+@InterfaceType()
+export abstract class IDepartment {
+  @Field(() => String)
   departmentName: string;
+  @Field(() => String)
   departmentDescription: string;
+  @Field(() => User, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
   departmentModerator?: IUser;
 }
