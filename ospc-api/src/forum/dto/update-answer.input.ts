1d8e26a868be4cd7d35120cf70ef572a757b2ad0
khaalid subaan <khaalidsubaan@gmail.com>
2021-02-12 22:49:50 +0800

    add answer service

--- ospc-api/src/forum/dto/update-answer.input.ts
+++ ospc-api/src/forum/dto/update-answer.input.ts
@@ -2,4 +2,6 @@ import { InputType, PartialType } from '@nestjs/graphql';
 import { Answer } from '../entities/answer.entity';
 
 @InputType()
-export class UpdateAnswerInput extends PartialType(Answer, InputType) {}
+export class UpdateAnswerInput extends PartialType(Answer, InputType) {
+  id: string;
+}
