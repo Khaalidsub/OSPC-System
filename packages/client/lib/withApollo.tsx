a97c99fc4e84b76a3d931c41065e929ee443889a
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2021-04-17 21:17:54 +0800

    Update config

--- packages/client/lib/withApollo.tsx
+++ packages/client/lib/withApollo.tsx
@@ -1,10 +1,7 @@
-import withApollo from 'next-with-apollo'
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client'
-import Cookies from 'universal-cookie';
-import cookie from "cookie";
-import { authHttpLink } from './utils';
-const url = process.env.NEXT_PUBLIC_URL || 'localhost:3001/graphql';
+import {  HttpLink } from '@apollo/client'
+const host = process.env.NEXT_PUBLIC_BACKEND || 'localhost'
+const port = process.env.NEXT_PUBLIC_BACKEND_PORT || '3001'
+ const url = process.env.NEXT_PUBLIC_URL || 'localhost:3001/graphql';
 export const httpLink = new HttpLink({
     uri: `http://${url}`,
-    credentials: 'include'
 })
