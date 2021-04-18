5616b4dffd7b58deb3f7108a1378f0003b8d9140
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2021-04-18 14:24:44 +0800

    Update to render the pages properly

--- packages/client/Layouts/MainLayout.tsx
+++ packages/client/Layouts/MainLayout.tsx
@@ -4,12 +4,18 @@ import { ProfileInfoCard } from 'components'
 import { getUserFromCookie } from 'lib/utils'
 import { useRouter } from 'next/router'
 import { CoachingStatus } from '__generated__/globalTypes'
+import { useQuery } from '@apollo/client'
+import { CURRENT_USER } from 'utililites/schema'
+import { currentUser } from 'utililites/__generated__/currentUser'
 export function MainLayout(props: MainLayoutProps) {
-
+    const {data:user,loading} = useQuery<currentUser>(CURRENT_USER)
+    if (loading) {
+        return <></>
+    }
     const router = useRouter()
-    const user = getUserFromCookie()
+    // const user = getUserFromCookie()
     const RenderProfile = () => {
-        if (router.pathname === '/' || router.pathname === '/login' || router.pathname === '/register' || router.pathname === '/pending') {
+        if (router.pathname === '/' || router.pathname === '/login' || router.pathname === '/register') {
             return <></>
         }
         return (
@@ -21,9 +27,9 @@ export function MainLayout(props: MainLayoutProps) {
     return (
         <div id='modal' className="min-h-screen relative   bg-gray-50 ">
             <div className=" relative min-h-screen flex flex-col-reverse md:justify-items-stretch md:flex-row">
-                <Navigation />
+                <Navigation currentUser={user?.currentUser} />
                 <div className="w-full space-y-8">
-                    {<RenderProfile />}
+                    <RenderProfile />
                     {/* <RenderProfile /> */}
                     {props.children}
 
