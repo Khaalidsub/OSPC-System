25a790393cde4150ea8c32ad118fd55e1f384b61
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2021-04-19 01:55:55 +0800

    Update uri to switch based one node env

--- packages/client/lib/withApollo.tsx
+++ packages/client/lib/withApollo.tsx
@@ -1,7 +1,7 @@
 import {  HttpLink } from '@apollo/client'
-const host = process.env.NEXT_PUBLIC_BACKEND || 'localhost'
-const port = process.env.NEXT_PUBLIC_BACKEND_PORT || '3001'
- const url = process.env.NEXT_PUBLIC_URL || 'localhost:3001/graphql';
+// const host = process.env.NEXT_PUBLIC_BACKEND || 'localhost'
+// const port = process.env.NEXT_PUBLIC_BACKEND_PORT || '3001'
+//  const url = process.env.NEXT_PUBLIC_URL || 'localhost:3001/graphql';
 export const httpLink = new HttpLink({
-    uri: `http://${url}`,
+    uri: process.env.NODE_ENV === 'production'?  `https://${process.env.NEXT_PUBLIC_URL}` : `http://localhost:3001/graphql`,
 })
