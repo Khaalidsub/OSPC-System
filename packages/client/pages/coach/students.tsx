37d91ee6bafd0ef3a470f56660bb876515d0352d
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2021-04-19 01:44:57 +0800

    Upate students page

--- packages/client/pages/coach/students.tsx
+++ packages/client/pages/coach/students.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@apollo/client'
 import { SearchField, SecondarySelectField } from 'components/InputFields'
 import { withAuth } from 'components/withAuth'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { COACH_LESSONS, STUDENT_LESSONS } from 'utilities/schema'
 import { studentLessons, studentLessons_studentLessons } from 'utilities/__generated__/studentLessons'
 // import { } fr
@@ -13,60 +13,76 @@ const MetricCard = () => {
         </div>
     )
 }
+
+
+const LessonOptions = ({search,setSearch}) => {
+    return (
+        <div className="grid grid-cols-1 w-1/2 mx-auto">
+
+
+                <SearchField placeholder="search students" search={search} setSearch={setSearch} />
+           
+        </div>
+    )
+}
 export const Lessons = () => {
     console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
-    const { data: coachLessons } = useQuery<studentLessons>(STUDENT_LESSONS)
+    const { data: coachLessonsData } = useQuery<studentLessons>(STUDENT_LESSONS)
+    const [search, setSearch] = useState('')
+    const [coachLessons, setCoachLessons] = useState([] as studentLessons_studentLessons[])
+
+    useEffect(() => {
+        // console.log('data', coachLessonsData);
+
+        setCoachLessons(coachLessonsData?.studentLessons)
+    }, [coachLessonsData])
+    useEffect(() => {
+        const result = coachLessonsData?.studentLessons?.filter((coach) => {
+            return coach.name.toLowerCase().includes(search)
+        })
+        setCoachLessons(result)
+    }, [search])
+
+    const LessonList = () => {
+        return (
+            <div className="flex flex-col space-y-6">
+                {coachLessons?.map((coachLesson) => {
+    
+                    return <Lesson {...coachLesson} key={coachLesson.id} />
+                })}
+            </div>
+    
+        )
+    }
     const Lesson = ({ email, id, name, lessons_given, }: studentLessons_studentLessons) => {
         return (
             <div className="relative flex flex-row bg-white  justify-between rounded-lg shadow-md  p-4 space-y-4">
                 <div className="flex flex-row w-full space-x-4 items-center">
-
+    
                     <img className="h-28 w-28 rounded-full" src="/fake_images/fake_user.png" alt="" />
                     <div className="flex flex-col   space-y-2">
-
+    
                         <div className="flex flex-row justify-between">
-
+    
                             <h2 className="font-poppins text-2xl" >{name}</h2>
                         </div>
-
+    
                         {/* <h4>{subjectSpecialization.specialization.map(special => { return special.title })}</h4> */}
-
+    
                     </div>
                 </div>
-
+    
                 <div className="flex flex-col w-1/2  justify-between items-center space-y-3 ">
-
+    
                     <h2 className="font-poppins text-xl" >{lessons_given} Lessons</h2>
-
-
+    
+    
                 </div>
-
+    
             </div>
         )
     }
-    const LessonList = () => {
-        return (
-            <div className="flex flex-col space-y-6">
-                {coachLessons?.studentLessons.map((coachLesson) => {
-
-                    return <Lesson {...coachLesson} key={coachLesson.id} />
-                })}
-            </div>
-
-        )
-    }
-    const LessonOptions = () => {
-        return (
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-3 ">
-                <SecondarySelectField label="" data={[]} />
-                <SecondarySelectField label="" data={[]} />
-                <div className='col-span-2'>
-
-                    <SearchField placeholder="search lessons" search={''} setSearch={() => { }} />
-                </div>
-            </div>
-        )
-    }
+ 
     const LessonResults = () => {
         return (
             <div className="flex flex-row justify-center">
@@ -82,8 +98,8 @@ export const Lessons = () => {
         <div className="grid grid-cols-1 gap-2">
             <div className="w-3/4 place-self-center space-y-4">
                 <LessonResults />
-                <LessonOptions />
-                <LessonList />
+                <LessonOptions search={search} setSearch={setSearch} />
+                <LessonList  />
             </div>
         </div>
     )
